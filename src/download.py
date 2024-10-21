16cf57a84f2d807fc37ebc846d7a923cf825ad14
francoaranda <frannco.aranda07@gmail.com>
2024-10-20 23:30:19 -0300

    se actualizó a yt_dlp pq el otro tenía errores de descarga

--- src/download.py
+++ src/download.py
@@ -1,28 +1,32 @@
-import youtube_dl #libreria que permite descargar y convertir los videos
+import yt_dlp as youtube_dl
 import os
-import sys
 import re
 import time
-#funcion para extrer el audio del video
-def extractSound(link):
 
+# función para extraer el audio del video
+def extractSound(link):
     while True:
         try:
+            # Cambiar youtube_dl.YoutubeDL() a yt_dlp.YoutubeDL()
             dataSound = youtube_dl.YoutubeDL().extract_info(url=link, download=False)
             title = dataSound['title']
-            title = re.sub(r'[^\w\s]', '', title)
+            title = re.sub(r'[^\w\s]', '', title)  # Eliminar caracteres especiales del título
             fileSound = f"{title}.webm"
             path_to_save = './webm'
             output_path = os.path.join(path_to_save, fileSound)
+
+            # Opciones para yt-dlp
             options = {
                 'format': 'bestaudio/best',
                 'keepvideo': False,
                 'outtmpl': output_path,
             }
 
+            # Descargar el audio usando yt-dlp
             with youtube_dl.YoutubeDL(options) as ydl:
                 return ydl.download([dataSound['webpage_url']])
         except youtube_dl.utils.DownloadError as e:
             print(f"Error durante la descarga: {e}")
             print("🔁 Reintentando la descarga en 5 segundos...")
             time.sleep(5)
+
